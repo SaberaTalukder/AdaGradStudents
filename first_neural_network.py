72c3beb7fb939d025613e1c5d9a03a685ecf6bb9
Sabera Talukder <saberatalukder@Saberas-MacBook-Pro.local>
2020-02-10 23:05:21 -0800

    sabera changes to first neural network

--- first_neural_network.py
+++ first_neural_network.py
@@ -10,18 +10,29 @@ from orderbookmodel import OrderbookModel
 class first_neural_network(OrderbookModel):
 
     def __init__(self):
-        # number of input parameters is 15
-        model = nn.Sequential(
+        # number of input parameters is 14
+        self.model = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(15, 20),
+
+            nn.Linear(14, 100),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(20, 10)
+            nn.Dropout(0.1),
+
+            nn.Linear(100, 10),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+
+            nn.Linear(10, 1)
         )
 
     def fit(self, X, y):
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
         loss_fn = nn.CrossEntropyLoss()
+
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+        
         pass
 
     def predict(self, X, y):
