448169bc9bf7fb74a84cc7bf522c5623dcfe9e4d
George Barnum <GBarnum@users.noreply.github.com>
2020-02-11 14:56:52 -0700

    Testing now can run through the process, including kfold validation

--- get_average_val_err.py
+++ get_average_val_err.py
@@ -1,5 +1,4 @@
 import numpy as np
-from orderbookmodel import OrderBookModel
 from sklearn.metrics import log_loss
 from sklearn.model_selection import KFold
 
@@ -23,9 +22,9 @@ def get_val_err(num_folds, train, model):
         print('Fold ', i, ' of ', num_folds, ' test indices:', val_index_list)
         print(len(val_index_list))
         # Training and testing data points for this fold:
-        x_train, x_val = train.drop(['id','y'], axis=1).iloc[train_index_list], train.drop('y', axis=1).iloc[val_index_list]
+        x_train, x_val = train.drop(['id','date','y'], axis=1).iloc[train_index_list], train.drop(['id','y','date'], axis=1).iloc[val_index_list]
         y_train, y_val = train[['y']].iloc[train_index_list], train[['y']].iloc[val_index_list]
-
+        
         model.fit(x_train, y_train)
         y_pred = model.predict(x_val)
         val_err = log_loss(y_val, y_pred)
