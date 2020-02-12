54f1bab072ea71877238ee43a3d5e5676c8dddfc
Sabera Talukder <saberatalukder@Saberas-MacBook-Pro.local>
2020-02-11 17:23:46 -0800

    adding the first_neural_network filled in

--- first_neural_network.py
+++ first_neural_network.py
@@ -1,9 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from orderbookmodel import OrderbookModel
 
@@ -24,16 +20,60 @@ class first_neural_network(OrderbookModel):
 
             nn.Linear(10, 1)
         )
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.learning_rate = 1e-3
+        self.batch_s = 32
+        self.num_epochs = 10
+
 
     def fit(self, X, y):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
-        loss_fn = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        loss_fn = self.loss_fn
 
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
-        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=True)
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_s, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=self.batch_s, shuffle=True)
 
-        
-        pass
+        self.model.train()
+
+        for epoch in (range(self.num_epochs)):
+            # data is a tensor here
+            for batch_idx, (data, target) in enumerate(train_loader):
+                # Erase accumulated gradients
+                optimizer.zero_grad()
+
+                # Forward pass
+                output = self.model(data)
+
+                # Calculate loss
+                loss = loss_fn(output, target)
+
+                # Backward pass
+                loss.backward()
+
+                # Weight update
+                optimizer.step()
+
+            # Track loss each epoch
+            print('Train Epoch: %d  Loss: %.4f' % (epoch + 1, loss.item()))
+
 
     def predict(self, X, y):
-        pass
+        self.model.eval()
+        loss_fn = self.loss_fn
+
+        test_loss = 0
+        correct = 0
+
+        # Turning off automatic differentiation
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = self.model(data)
+                test_loss += loss_fn(output, target).item()  # Sum up batch loss
+                pred = output.argmax(dim=1, keepdim=True)  # Get the index of the max class score
+                correct += pred.eq(target.view_as(pred)).sum().item()  # calculates accuracy for classification
+
+        test_loss /= len(test_loader.dataset)
+
+        print('Test set: Average loss: %.4f, Accuracy: %d/%d (%.4f)' %
+              (test_loss, correct, len(test_loader.dataset),
+               100. * correct / len(test_loader.dataset)))
