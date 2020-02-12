eb8245718089d3bd3916156c4a30de1556922382
George Barnum <GBarnum@users.noreply.github.com>
2020-02-11 17:23:47 -0700

    using the model framework

--- randomforestorder.py
+++ randomforestorder.py
@@ -1,17 +1,18 @@
-from orderbookmodel import OrderBookModel
 from sklearn import ensemble
 
 
 
-class RandomForestOrder(OrderBookModel):
+class RandomForestOrder():
     
     def __init__(self):
-        model = ensemble.RandomForestClassifier()
+        
+        self.model = ensemble.RandomForestClassifier(n_estimators=100,max_depth=10)
         
         
     def fit(self, X,y):
         self.model.fit(X,y)
 
     def predict(self, X):
-        return self.model.predict(X)
+        #return self.model.predict(X)
+        return self.model.predict_proba(X)[:,1]
 
