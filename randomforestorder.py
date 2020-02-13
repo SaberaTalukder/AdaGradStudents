fixing normalization

randomforestorder.py
@@ -20,12 +20,18 @@ class RandomForestOrder():
         return normalized_X, mean_val, std_dev
 
     def fit(self, X,y):
+
         X, mean_val, std_dev = self.normalize_vals(X)
+        self.mean_val = mean_val
+        self.std_dev = std_dev
 
         self.model.fit(X,y)
 
     def predict(self, X):
         #return self.model.predict(X)
-        X, mean_val, std_dev = self.normalize_vals(X)
+        #X, mean_val, std_dev = self.normalize_vals(X)
+        
+        X = np.asarray(X)
+        X = (X - self.mean_val) / self.std_dev
         return self.model.predict_proba(X)[:,1]
 